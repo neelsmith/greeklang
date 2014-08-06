c46d8570ce954591034376c9dfb53a4b465f37b3
Neel Smith <dnsmith.neel@gmail.com>
2014-08-05 21:29:13 -0400

    Add standard skeleton for a groovy/maven build.

---
+++ src/main/groovy/edu/holycross/shot/jmorph/GreekString.groovy
@@ -0,0 +1,4 @@
+package edu.holycross.shot.jmorph
+
+class GreekString {
+}
---
+++ src/test/groovy/edu/holycross/shot/jmorph/TestGreekString.groovy
@@ -0,0 +1,13 @@
+package edu.holycross.shot.jmorph
+
+
+import static org.junit.Assert.*
+import org.junit.Test
+
+
+class TestGreekString extends GroovyTestCase {
+
+  void testString() {
+  }
+
+}
