4797a5bfbed7e77716bfa519f55d5dc0244fee3a
Neel Smith <dnsmith.neel@gmail.com>
2014-08-05 22:14:04 -0400

    Back to the future.  Start replicating 1985 tests on character classes.

--- src/main/groovy/edu/holycross/shot/jmorph/GreekString.groovy
+++ src/main/groovy/edu/holycross/shot/jmorph/GreekString.groovy
@@ -1,4 +1,84 @@
 package edu.holycross.shot.jmorph
 
+/**
+ * A class for working with a Greek string represented
+ * in a subset of the TLG's beta code.
+ *
+ * Valid characters are:
+ * alphabetic
+ * 
+ */
 class GreekString {
+  
+  def consonant = [
+    'b','g','d',
+    'z','q','k',
+    'l','m','n','c',
+    'p','r','s','t',
+    'f','x','y'
+  ]
+  def vowel = [
+    'a', 'e','h','i','o',
+    'u','w'
+  ]
+  def breathing = [')','(']
+  def accent = ['/','\\','=']
+  def quantity = ['_','^']
+
+
+
+  /** Constructor verifies that
+   * String contains only valid characters.
+   * @param betaString Greek string, in beta code.
+   * @throws Exception if not all characters in betaString are valid.
+   */
+  GreekString(String betaString) 
+  throws Exception {
+    Integer count = 0
+    while (count < betaString.length() - 1) {
+      if (!(validChar(betaString.substring(count,count+1)))) {
+	System.err.println "Error parsing ${betaString}: failed on ${betaString.substring(count,count+1)} (char ${count})"
+	throw new Exception("GreekString: invalid characer ${betaString.substring(count,count+1)}")
+      }
+      count++
+    }
+  }
+
+
+  /** Determines if a one-character long string is a valid GreekString
+   * character.
+   * @param ch String to check.
+   * @returns true if character is valid, otherwise false.
+   */ 
+  boolean validChar(String ch) {
+    if (
+      (breathing.contains(ch))
+      || (isAlphabetic(ch)) 
+      || (accent.contains(ch)) 
+      || (quantity.contains(ch))
+    )  {
+      return true
+    } else {
+      return false
+    }
+  }
+
+
+
+  /** Determines if a one-character long string is an alphabetic
+   * character.
+   * @param ch String to check.
+   * @returns true if character is alphabetic, otherwise false.
+   */ 
+  boolean isAlphabetic(ch) {
+    if (
+      (consonant.contains(ch))
+      || (vowel.contains(ch))
+    ) {
+      return true
+    } else  {
+      return false 
+    }
+  }
+
 }
--- src/test/groovy/edu/holycross/shot/jmorph/TestGreekString.groovy
+++ src/test/groovy/edu/holycross/shot/jmorph/TestGreekString.groovy
@@ -7,7 +7,28 @@ import org.junit.Test
 
 class TestGreekString extends GroovyTestCase {
 
-  void testString() {
+  void testConstructor() {
+    GreekString gkstr = new GreekString("mh=nin")
+    assert gkstr
+    
+    assert shouldFail {
+      GreekString bogus= new GreekString("123")
+    }
+  }
+
+
+  void testCharClass() {
+    GreekString gkstr = new GreekString("mh=nin")
+
+    gkstr.consonant.each { c ->
+      assert gkstr.isAlphabetic(c)
+    }
+
+    gkstr.vowel.each { v ->
+      assert gkstr.isAlphabetic(v)
+    }
+
+    
   }
 
 }
