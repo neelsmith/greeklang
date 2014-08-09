567376fe01cc15e6209fef0d42d81a7f782d1822
Neel Smith <dnsmith.neel@gmail.com>
2014-08-09 09:14:41 -0400

    Test that encoded constructor handles bad input properly.

---
+++ src/test/groovy/edu/holycross/shot/greekutils/TestStringEncodings.groovy
@@ -0,0 +1,31 @@
+package edu.holycross.shot.greekutils
+
+
+import static org.junit.Assert.*
+import org.junit.Test
+
+
+class TestStringEncodings extends GroovyTestCase {
+
+
+
+  void testUnicode() {
+    String testUnicode = "μῆνιν"
+    String testBeta = "mh=nin"
+
+    GreekString uniGreek = new GreekString(testUnicode, "Unicode")
+    GreekString betaGreek = new GreekString(testBeta)
+
+    assert uniGreek.toString() == betaGreek.toString()
+  }
+
+  void testLimits() {
+    String notGreek = "αλφα = 1"
+    assert shouldFail {
+      GreekString badString = new GreekString(notGreek, "Unicode")
+    }
+  }
+
+
+
+}
