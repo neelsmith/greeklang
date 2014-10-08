da22919b003ef08c19ce3f8fca246559592bdbdf
Neel Smith <dnsmith.neel@gmail.com>
2014-10-08 07:19:40 -0400

    Make separate package for tokens.

--- specs/specs/greek/tokens/GreekWordFixture.java
+++ specs/specs/greek/tokens/GreekWordFixture.java
@@ -1,4 +1,4 @@
-package specs.greek;
+package specs.greek.tokens;
 
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
---
+++ specs/specs/greek/tokens/TokensTest.java
@@ -0,0 +1,7 @@
+package specs.greek.tokens;
+
+
+import org.concordion.integration.junit3.ConcordionTestCase;
+
+public class TokensTest extends ConcordionTestCase {
+}
