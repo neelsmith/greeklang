6b8ceeb70be67024dd6aeeba6e4fc63188586c9e
Neel Smith <dnsmith.neel@gmail.com>
2014-10-08 11:04:20 -0400

    Fix name of class.

--- specs/specs/greek/syllables/SyllableTest.java
+++
@@ -1,7 +0,0 @@
-package specs.greek.syllable;
-
-
-import org.concordion.integration.junit3.ConcordionTestCase;
-
-public class SyllableTest extends ConcordionTestCase {
-}
---
+++ specs/specs/greek/syllables/SyllablesTest.java
@@ -0,0 +1,7 @@
+package specs.greek.syllables;
+
+
+import org.concordion.integration.junit3.ConcordionTestCase;
+
+public class SyllablesTest extends ConcordionTestCase {
+}
