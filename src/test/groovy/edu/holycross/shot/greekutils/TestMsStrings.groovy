11ebe10776466bfcf69fef13c28b5d1d9324b420
Neel Smith <dnsmith.neel@gmail.com>
2015-06-20 17:11:40 -0400

    Implement GreekMsString class

---
+++ src/test/groovy/edu/holycross/shot/greekutils/TestMsStrings.groovy
@@ -0,0 +1,31 @@
+package edu.holycross.shot.greekutils
+
+
+import static org.junit.Assert.*
+import org.junit.Test
+
+/** Class tests extracting integer and fraction
+ * components of a MilesianString.
+ */
+class TestMsStrings extends GroovyTestCase {
+
+
+
+  
+  void testInstance() {
+    String eoSchol = "⁑"
+    GreekMsString greekEoSchol = new GreekMsString(eoSchol, "Unicode")
+    assert greekEoSchol.toString() == eoSchol
+  }
+
+
+
+  void testCombos() {
+    String macronCombo = "ἀ̄λλ'"
+    GreekMsString greekMacronCombo = new GreekMsString(macronCombo, "Unicode")
+    println "${macronCombo} -> ${greekMacronCombo.toString(false)} "
+    //   assert greekMacronCombo.toString() == macronCombo
+  }
+
+
+}
