d2f51bfc362894b1cb9bf1138b179eea90fcf7c4
Neel Smith <dnsmith.neel@gmail.com>
2015-09-29 05:53:57 -0400

    Add placeholder for script

---
+++ morphology/genAnalysisUrns.groovy
@@ -0,0 +1,6 @@
+// generate URNs for morphological analyses
+// do this on morphanalysis objects!
+//
+// verbs: cycle all values of PNTMV
+// nouns:  cycle all values of GCN
+// adjs:  cycle all values of GCND
