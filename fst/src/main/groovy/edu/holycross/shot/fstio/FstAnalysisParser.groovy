c6f2c4b96111a31fe116fe10a2c53d11474f9add
Neel Smith <dnsmith.neel@gmail.com>
2015-10-06 11:45:38 -0400

    Update fstanalysis for verb parsing

--- fst/src/main/groovy/edu/holycross/shot/fstio/FstAnalysisParser.groovy
+++ fst/src/main/groovy/edu/holycross/shot/fstio/FstAnalysisParser.groovy
@@ -16,6 +16,18 @@ class FstAnalysisParser {
   String pos
   // to derive from tags:
   String morphAnalysis
+  //
+  def stemTags
+  def inflTags
+
+
+  // move this into separate abstract class...
+
+  def pers = ["<1st>": 0]
+  def num = ["<sg>": 0]
+  def tense = ["<pres>": 0]
+  def mood = ["<indic>": 0]
+  def voice = ["<act>": 0]
 
 
   /** Constructor. */
@@ -24,23 +36,40 @@ class FstAnalysisParser {
       stemString = cols[0]
       inflectionString = cols[1]
       // check that strings are not null before doing findAll
-      def stemTags = stemString.findAll(/<[^>]+>/)
-      def infTags = inflectionString.findAll(/<[^>]+>/)
+       stemTags = stemString.findAll(/<[^>]+>/)
+       inflTags = inflectionString.findAll(/<[^>]+>/)
 
       stem = stemTags[0]
       lexicalEntity = stemTags[1]
-      inflectionalPattern  = infTags[1]
+      inflectionalPattern  = inflTags[1]
       if (stemTags[2] != "<#>") {
         pos = stemTags[2]
       } else {
-        if (["<infin>", "<ptcpl>","<vadj>"].contains(infTags[1])) {
-          pos = infTags[1]
+        if (["<infin>", "<ptcpl>","<vadj>"].contains(inflTags[1])) {
+          pos = inflTags[1]
         } else {
           pos = "<verb>"
         }
       }
-
+      morphAnalysis = computeMorphAnalysis()
       //  "<coretests.n64316_0><lexent.n64316><#>lu<verb><w_regular>::<w_regular><w_indicative.1>w<1st><sg><pres><indic><act>"
   }
+
+  String computeMorphAnalysis() {
+    String urnBase = "urn:cite:morph:form"
+    switch (pos) {
+      case "<verb>":
+      System.err.println "Analyze verb"
+      // check size of inflTags...
+      return "${urnBase}.cv" + pers[inflTags[2]] + num[inflTags[3]] + tense[inflTags[4]] + mood[inflTags[5]] + voice[inflTags[6]]
+      break
+
+      default:
+      System.err.println "Don't know how to parse PoS " + pos
+      break
+    }
+  }
+
+
 
 }
--- fst/src/test/groovy/edu/holycross/shot/fstio/TestAnalysisParser.groovy
+++ fst/src/test/groovy/edu/holycross/shot/fstio/TestAnalysisParser.groovy
@@ -9,13 +9,13 @@ class TestAnalysisParser {
 
   @Test
   void testParser() {
-    String infin = "<coretests.n64316_0><lexent.n64316><#>lu<verb><w_regular>::<w_regular><w_indicative.1>w<1st><sg><pres><indic><act>"
-    FstAnalysisParser fap = new FstAnalysisParser(infin)
+    String conjverb = "<coretests.n64316_0><lexent.n64316><#>lu<verb><w_regular>::<w_regular><w_indicative.1>w<1st><sg><pres><indic><act>"
+    FstAnalysisParser fap = new FstAnalysisParser(conjverb)
     assert fap.getStem() == "<coretests.n64316_0>"
     assert fap.getLexicalEntity() == "<lexent.n64316>"
     assert fap.getInflectionalPattern() == "<w_indicative.1>" ///"<w_regular><w_indicative.1>w<1st><sg><pres><indic><act>"
     assert fap.getPos() == "<verb>"
-
+    assert fap.getMorphAnalysis() == "urn:cite:morph:form.cv00000"
 
   }
 }
