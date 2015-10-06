a102063e73488acd4be842c3c7f0fc018d3c77ea
Neel Smith <dnsmith.neel@gmail.com>
2015-10-06 10:16:17 -0400

    Initial parsing of FST output

--- fst/src/main/groovy/edu/holycross/shot/fstio/FstAnalysisParser.groovy
+++ fst/src/main/groovy/edu/holycross/shot/fstio/FstAnalysisParser.groovy
@@ -6,15 +6,41 @@ package edu.holycross.shot.fstio
 class FstAnalysisParser {
 
 
+  String stemString
+  String inflectionString
+
+  // all URNs, really
+  String lexicalEntity
   String stem
-  String inflection
+  String inflectionalPattern
+  String pos
+  // to derive from tags:
+  String morphAnalysis
+
 
   /** Constructor. */
   FstAnalysisParser(String analysisStr) {
       def cols = analysisStr.split(/::/)
-      stem = cols[0]
-      inflection = cols[1]
-    // do nothing
+      stemString = cols[0]
+      inflectionString = cols[1]
+      // check that strings are not null before doing findAll
+      def stemTags = stemString.findAll(/<[^>]+>/)
+      def infTags = inflectionString.findAll(/<[^>]+>/)
+
+      stem = stemTags[0]
+      lexicalEntity = stemTags[1]
+      inflectionalPattern  = infTags[1]
+      if (stemTags[2] != "<#>") {
+        pos = stemTags[2]
+      } else {
+        if (["<infin>", "<ptcpl>","<vadj>"].contains(infTags[1])) {
+          pos = infTags[1]
+        } else {
+          pos = "<verb>"
+        }
+      }
+
+      //  "<coretests.n64316_0><lexent.n64316><#>lu<verb><w_regular>::<w_regular><w_indicative.1>w<1st><sg><pres><indic><act>"
   }
 
 }
--- fst/src/test/groovy/edu/holycross/shot/fstio/TestAnalysisParser.groovy
+++ fst/src/test/groovy/edu/holycross/shot/fstio/TestAnalysisParser.groovy
@@ -3,14 +3,19 @@ package edu.holycross.shot.fstio
 import org.junit.Test
 import static groovy.test.GroovyAssert.shouldFail
 
+//<coretests.n64316_0><lexent.n64316><#>lu<verb><w_regular>::<w_regular><w_indicative.1>w<1st><sg><pres><indic><act>
 
 class TestAnalysisParser {
 
   @Test
   void testParser() {
-    String infin = "<n64316><#>lu<verb><w_regular>::<infin>ein<pres><act><penacc>"
+    String infin = "<coretests.n64316_0><lexent.n64316><#>lu<verb><w_regular>::<w_regular><w_indicative.1>w<1st><sg><pres><indic><act>"
     FstAnalysisParser fap = new FstAnalysisParser(infin)
-    assert fap.stem == "<n64316><#>lu<verb><w_regular>"
-    assert fap.inflection == "<infin>ein<pres><act><penacc>"
+    assert fap.getStem() == "<coretests.n64316_0>"
+    assert fap.getLexicalEntity() == "<lexent.n64316>"
+    assert fap.getInflectionalPattern() == "<w_indicative.1>" ///"<w_regular><w_indicative.1>w<1st><sg><pres><indic><act>"
+    assert fap.getPos() == "<verb>"
+
+
   }
 }
