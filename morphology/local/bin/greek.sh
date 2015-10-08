edbf22f8348079f8b06de4c61ee33349c09fd6d5
Neel Smith <dnsmith.neel@gmail.com>
2015-10-08 05:23:44 -0400

    Add placeholders for CLI scripts

---
+++ morphology/local/bin/greek.sh
@@ -0,0 +1,8 @@
+# bash script to invoke groovy cli.
+# default for binary is /usr/local/morphology/greek.a
+# Options to support:
+# -p --parser for alternate binary
+# output options:
+# -u -urns. URN output only
+# -e --explain. Include second-tier urns
+# -l --legible. Human-readable output
---
+++ morphology/local/morphology/greek.groovy
@@ -0,0 +1,10 @@
+/*
+groovy script to run fst-infl
+
+Options to support:
+
+# -u -urns. URN output only
+# -e --explain. Include second-tier urns
+# -l --legible. Human-readable output
+
+*/
