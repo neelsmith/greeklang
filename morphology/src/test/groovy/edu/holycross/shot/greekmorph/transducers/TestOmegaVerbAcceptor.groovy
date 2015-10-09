50e8bc96f36d566c18d4849a7a9b6e4ed007dbb0
Neel Smith <dnsmith.neel@gmail.com>
2015-10-09 06:30:20 -0400

    Unit test verb acceptor

--- morphology/src/test/groovy/edu/holycross/shot/greekmorph/transducers/TestOmegaVerbAcceptor.groovy
+++ morphology/src/test/groovy/edu/holycross/shot/greekmorph/transducers/TestOmegaVerbAcceptor.groovy
@@ -11,33 +11,48 @@ class TestOmegaVerbAcceptor {
   String fstinfl = "/usr/bin/fst-infl"
   String transducer = "build/fst/acceptors/verb.a"
   File testFile = new File("build/testInput.txt")
+  String cmd = "${fstinfl} ${transducer} ${testFile}"
+
+  ArrayList getAnalysisStrings() {
+    def analysisStrings = []
+    Process process = cmd.execute()
+    def out = new StringBuffer()
+    def err = new StringBuffer()
+    process.consumeProcessOutput( out, err )
+    process.waitFor()
+    out.toString().eachLine { l ->
+      if (l[0] == ">") {
+        // omit
+      } else if (l ==~ /No result.+/) {
+        // omit
+      } else {
+        FstAnalysisParser fsp = new FstAnalysisParser(l)
+        MorphForm morphForm = fsp.getMorphForm()
+        //AnalysisExplanation explanation = fsp.getExplanation()
+        analysisStrings.add(morphForm.toString())
+      }
+    }
+    return analysisStrings
+  }
+
   @Test
   void testVerbAcceptor() {
-    def testList = ["<coretests.n64316_0><lexent.n64316><#>lu<verb><w_regular>::<w_regular><w_indicative.1>w<1st><sg><pres><indic><act>",
-    "<coretests.n64316_0><lexent.n64316><#>lu<verb><w_regular>::<w_regular><w_indicative.7>omai<1st><sg><pres><indic><mid>",
-    "<coretests.n64316_0><lexent.n64316><#>lu<verb><w_regular>::<w_regular><w_indicative.7>omai<1st><sg><pres><indic><pass>"]
-    
-    testList.each { wd ->
-      testFile.setText(wd)
-      String cmd = "${fstinfl} ${transducer} ${testFile}"
-      Process process = cmd.execute()
-      def out = new StringBuffer()
-      def err = new StringBuffer()
-      process.consumeProcessOutput( out, err )
-      process.waitFor()
-      out.toString().eachLine { l ->
-        if (l[0] == ">") {
-          // omit
-        } else if (l ==~ /No result.+/) {
-          // omit
-        } else {
-          FstAnalysisParser fsp = new FstAnalysisParser(l)
-          MorphForm morphForm = fsp.getMorphForm()
-          AnalysisExplanation explanation = fsp.getExplanation()
-          println morphForm
-          println explanation
-        }
-      }
+    def testMap = [
+    "<coretests.n64316_0><lexent.n64316><#>lu<verb><w_regular>::<w_regular><w_indicative.1>w<1st><sg><pres><indic><act>":
+    ["conjugated verb: first person singular present indicative active"],
+
+    "<coretests.n64316_0><lexent.n64316><#>lu<verb><w_regular>::<w_regular><w_indicative.7>omai<1st><sg><pres><indic><mid>":
+    ["conjugated verb: first person singular present indicative middle"],
+
+    "<coretests.n64316_0><lexent.n64316><#>lu<verb><w_regular>::<w_regular><w_indicative.7>omai<1st><sg><pres><indic><pass>":
+    ["conjugated verb: first person singular present indicative passive"]
+
+    ]
+
+    testMap.each { wd ->
+      testFile.setText(wd.key)
+      def actualReplies = getAnalysisStrings()
+      assert actualReplies as Set ==  wd.value as Set
     }
   }
 }
