b2024c07ae75ef5e1ab61dd45496f9126a18813c
Neel Smith <dnsmith.neel@gmail.com>
2015-10-10 10:34:48 -0400

    Correct data format

--- morphology/src/test/groovy/edu/holycross/shot/greekmorph/TestUrnManager.groovy
+++ morphology/src/test/groovy/edu/holycross/shot/greekmorph/TestUrnManager.groovy
@@ -8,14 +8,10 @@ class TestUrnManager {
 
   File csvSource = new File("src/fst/collectionAbbreviations.csv")
 
-
-
-
-
   @Test
   void testUrnMgr() {
     UrnManager umgr = new UrnManager(csvSource)
-    String expected = "urn:cite:shot:lexent:"
+    String expected = "urn:cite:shot:lexent"
     assert umgr.getUrn("lexent").toString() ==  expected
   }
 }
