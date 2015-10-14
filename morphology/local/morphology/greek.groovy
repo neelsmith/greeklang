5d553f44cf70806be3f53f2fdc56e0cc6b180c4a
Neel Smith <dnsmith.neel@gmail.com>
2015-10-14 13:15:26 -0400

    Add skeleton for groovy CLI

--- morphology/local/morphology/greek.groovy
+++ morphology/local/morphology/greek.groovy
@@ -1,24 +1,23 @@
 /*
 groovy script to run fst-infl
-
-Options to support:
-
-# -u -urns. URN output only
-# -e --explain. Include second-tier urns
-# -l --legible. Human-readable output
-
 */
 
-def cli = new CliBuilder(usage: 'greek.groovy -[uelp]')
 
-// Create the list of options.
+// defaults:
+
+String fstinfl = "/usr/bin/fst-infl"
+String transducer = "/usr/local/morphology/greek.a"
+
+def cli = new CliBuilder(usage: 'greek.groovy -[uelp]')
 cli.with {
   h longOpt: 'help', 'Show usage information'
   u longOpt: 'urn', 'Include analytical urns only'
   e longOpt: 'explain', 'Include explanatory URNs in output'
   l longOpt: 'legible', 'Include human-readable text in output'
 
-  p longOpt: 'parser', args: 1, argName: 'parser', 'Binary parser to use'
+  p longOpt: 'parser', args: 1, argName: 'parser', 'Binary transducer to use in parsing'
+  i longOpt: 'fstinfl', args: 1, argName: 'fstinfl', 'Path to fst-infl'
+
 }
 
 def options = cli.parse(args)
@@ -31,15 +30,21 @@ if (!options) {
 // Show usage text when -h or --help option is used.
 if (options.h) {
   cli.usage()
+  System.exit(0)
 }
 
 if (options.p) {
-  println "Use binary transducer " + options.p + " for parser"
+  transducer = options.p
+}
+if (options.i) {
+  fstinfl = options.i
 }
 
+String cmd = "${fstinfl} ${transducer}"
 
 if (extraArguments.size() > 0) {
   println "Parse files or directories"
 } else {
   println "Run interactively"
+  cmd.execute()
 }
