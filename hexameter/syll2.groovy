26eb1fdd4bc3cc57e6fb085425dbeaa37a5729e0
Neel Smith <dnsmith.neel@gmail.com>
2015-10-18 03:13:04 -0400

    More reg ex patterns

--- hexameter/syll2.groovy
+++ hexameter/syll2.groovy
@@ -1,13 +1,6 @@
 // syll2.groovy
 def wds = ["mhnin", "aeide", "qea", "phlhi+adew", "axilhos", "oulomenhn", "h", "muri'" ,"axaiois", "eqnos", "sofizw", "astron", "pragma", "sofizomenos"]
 
-java.util.regex.Pattern consonant = ~/[bgdzqklmncprstfxy]/
-
-java.util.regex.Pattern vowel = ~/[aeiouhw]/
-
-
-java.util.regex.Pattern diphth = ~/(ai|oi|ei)/
-
 
 // Smyth 140B
 // stop: pbftdqkgx
@@ -16,12 +9,12 @@ java.util.regex.Pattern stop_liq = ~/([aeiouhw\|])([bgdzqklmncprstfxy]*[pbftdqkg
 java.util.regex.Pattern mn = ~/([aeiouhw\|])mn/
 
 // add:
-
 // sigma rules
 // split (vowel + liquid) - consonant
 
-// split double consonants
-java.util.regex.Pattern dubble = ~/([bgdzqklmnprstfx]{2})/
+
+
+
 
 
 
@@ -31,8 +24,13 @@ java.util.regex.Pattern dubble = ~/([bgdzqklmnprstfx]{2})/
 
 // split diphthong-verb combos
 java.util.regex.Pattern dv = ~/(ai|oi|ei|au|eu|ou|hu|wu|ui)([aeiouhw])/
-java.util.regex.Pattern vd = ~/([aeiouhw])((ai|oi|ei|au|eu|ou|hu|wu|ui))/
+java.util.regex.Pattern vd = ~/([aeiouhw])(ai|oi|ei|au|eu|ou|hu|wu|ui)/
+// split double consonants
+java.util.regex.Pattern dubble = ~/(b{2}|g{2}|d{2}|z{2}|q{2}|k{2}|l{2}|m{2}|n{2}|p{2}|r{2}|s{2}|t{2}|f{2}|x{2})/
 
+
+
+// vowel-consonant-vowel
 java.util.regex.Pattern vcv = ~/([aeiouhw\|\+])([bgdzqklmncprstfxy][aeiouhw])/
 
 def testMap = [
@@ -42,25 +40,34 @@ def testMap = [
   "limnh" : "li#mnh",*/
   "alhqeia": "a#lh#qei#a",
   "qalatta" : "qa#lat#ta",
-  "anqrwpoi" : "an#qrw#poi"
+  "anqrwpoi" : "an#qrw#poi",
+  "aeide" : "a#ei#de",
+  "poios" : "poi#os"
   //"tuptw" : "tu#ptw",
   //"astron" : "a#stron"
 
 ]
+boolean verbose = true
 testMap.each { w ->
+  println "analyze " + w.key
 
+  // dipthong-vowel splits
   String sylls = w.key.replaceAll(dv) { fullMatch, dipth, vow ->
-    println "match data: " + fullMatch + ":" + dipth + ":" + vow
     dipth + "#" + vow
-
   }
+  if (verbose) { println "\tafter dv " + sylls }
+  // vowel-dipthong splits
+  sylls = sylls.replaceAll(vd) {fullMatch, vow, dipth ->
+    vow + "#" + dipth
+  }
+  if (verbose) { println "\tafter vd " + sylls }
 
-
+  // double consonants split
   sylls = sylls.replaceAll(dubble) { fullMatch, doubled ->
     doubled[0] + "#" + doubled[1]
   }
+  if (verbose) { println "\tafter dubble " + sylls }
 
-  println w.key + "-> " + sylls.split(/#/)
   /*
   String pass1 = w.key.replaceAll(vcv) { fullMatch, v, cv ->
     v + "#" + cv
@@ -75,6 +82,10 @@ testMap.each { w ->
     preceding + "#mn"
   }
   println w.key + "-> " + p3
-  assert w.value == p3
+
   */
+
+
+  println "\t" + w.key + "-> " + sylls//.split(/#/)
+  //assert w.value == sylls
 }
