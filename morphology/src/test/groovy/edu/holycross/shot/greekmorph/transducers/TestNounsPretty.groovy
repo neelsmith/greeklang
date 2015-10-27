76e0407fb382ca099c1a00cb0de236eb1ab2e9bd
Neel Smith <dnsmith.neel@gmail.com>
2015-10-27 14:57:15 -0400

    Test nouns didactically

--- morphology/src/test/groovy/edu/holycross/shot/greekmorph/transducers/TestNounsPretty.groovy
+++ morphology/src/test/groovy/edu/holycross/shot/greekmorph/transducers/TestNounsPretty.groovy
@@ -10,54 +10,70 @@ import static groovy.test.GroovyAssert.shouldFail
 */
 class TestNounsPretty {
 
-
+  // External files used in didactic tests:
+  // FST toolkit's batch parser:
   String fstinfl = "/usr/bin/fst-infl"
-  File inflCsvSource = new File("src/fst/collectionAbbreviations.csv")
+  // CSV files with URN abbreviations for stems and inflectional rules
   File lexCsvSource = new File("sampledata/urn-registries/datasets.csv")
-  //File testFile = new File("build/testInput.txt")
+  File inflCsvSource = new File("src/fst/collectionAbbreviations.csv")
 
 
   @Test
-  void testMorphParser() {
+  void testParserDidactically() {
     // You build a MorphologicalParser with a FST
     // and a URNManager:
     // 1. Compiled finite state transducer:
     String fstBinary = "build/fst/greek.a"
-    // 2. Configure a URN manager with CITE collectionAbbreviations
+    // 2. A URN manager configured with CITE collection abbreviations
     // for both inflectional patterns and lexicon of stems:
     UrnManager umgr = new UrnManager(inflCsvSource)
     umgr.addCsvFile(lexCsvSource)
 
     MorphologicalParser mp = new MorphologicalParser(fstBinary, umgr)
 
-    // MorphologicalParsers operate on GreekStrings:
+    // MorphologicalParsers can operate on GreekString objects:
     String testWord = "μῆνιν"
     GreekString s = new GreekString(testWord, "Unicode")
 
     // Parsing a GreekString gets you 0 or more analyses
     MorphologicalAnalysis morph = mp.parseGreekString(s)
-    morph.analyses.eachWithIndex { morphAnalysis, idx ->
-      // Individual analyses of a word have three components.
-      // (1) An analytical type:
-      assert morphAnalysis.analyticalType == AnalyticalType.NOUN
-      // (2) a citable form:
-      CitableForm form = morphAnalysis.getAnalysis()
-      // (since this form is a noun, it is guaranteed to have GCN:)
-      assert form.getGender() == Gender.FEMININE
-      assert form.getCas() == GrammaticalCase.ACCUSATIVE
-      assert form.getNum() == GrammaticalNumber.SINGULAR
-      // (of course there are pretty stringifications:)
-      assert morphAnalysis.toString() == "noun: feminine accusative singular"
-      assert form.toString() == "feminine accusative singular"
+    // although there is only 1 possibility for μῆνιν.
+    assert morph.analyses.size() == 1
 
-      // and (3) an explanation for the analysis (found at the same index
-      // in the parallel list of explanations):
-      AnalysisExplanation explanation = morph.explanations[idx]
-      // Stems are explained by a URN identified the relation of URN
-      //explain stem: urn:cite:gmorph:coretests.n67485_0
-//explain inflection: urn:cite:gmorph:nouninfl.is_ios4
-      System.err.println "explain stem: " + explanation.stem
-      System.err.println "explain inflection: " + explanation.inflection
+    morph.analyses.each { morphAnalysis ->
+      // Individual analyses of a word have three components.
+      // (1) The lexical entity :
+      String urnForMenis = "urn:cite:shot:lexent.n67485"
+      assert morphAnalysis.getLexicalEntity().toString() == urnForMenis
+
+      // (2) a form:
+      MorphForm form = morphAnalysis.getMorphForm()
+      // which has a defined type
+      assert form.getAnalyticalType() == AnalyticalType.NOUN
+
+      // and since this form is a noun, it is guaranteed to have GCN:
+      CitableId formIdentification = form.getAnalysis()
+      assert formIdentification.getGender() == Gender.FEMININE
+      assert formIdentification.getCas() == GrammaticalCase.ACCUSATIVE
+      assert formIdentification.getNum() == GrammaticalNumber.SINGULAR
+
+
+      // and (3) an explanation for the analysis
+      AnalysisExplanation explanation = morphAnalysis.getAnalysisExplanation()
+      // Stems are explained by a URN identifying the source for that
+      // stem in a lexicon:
+      String expectedStemExplanation =  "urn:cite:gmorph:coretests.n67485_0"
+      assert explanation.stem.toString() == expectedStemExplanation
+      // Inflectional patterns are explained by a URN identifying the
+      // the inflectional rule applied to the stem
+      String expectedInflectionExplanation = "urn:cite:gmorph:nouninfl.is_ios4"
+      assert explanation.inflection.toString() == expectedInflectionExplanation
+
+      // Of course there are various stringifications:
+      assert form.toString() == "noun: feminine accusative singular"
+      assert explanation.toString() == "stem urn:cite:gmorph:coretests.n67485_0, inflection   urn:cite:gmorph:nouninfl.is_ios4"
+      assert morphAnalysis.toString() == "from urn:cite:shot:lexent.n67485, noun: feminine accusative singular (stem urn:cite:gmorph:coretests.n67485_0, inflection urn:cite:gmorph:nouninfl.is_ios4)"
+
     }
   }
 
