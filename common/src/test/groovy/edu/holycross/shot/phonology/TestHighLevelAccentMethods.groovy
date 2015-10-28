17a6fb681101315224c58c88e3a03dff521b41cd
Neel Smith <dnsmith.neel@gmail.com>
2015-10-28 15:19:07 -0400

    Add unit test

--- common/src/test/groovy/edu/holycross/shot/phonology/TestHighLevelAccentMethods.groovy
+++ common/src/test/groovy/edu/holycross/shot/phonology/TestHighLevelAccentMethods.groovy
@@ -15,10 +15,7 @@ class TestHighLevelAccentMethods {
   void testRecessive() {
     GreekWord proparoxytone = new GreekWord("a)nqrwpos")
     String expectedProparoxytone = "a)/nqrwpos"
-    System.err.println "Want to add accent to " + proparoxytone
-    System.err.println "Use pattern " + AccentPattern.RECESSIVE
-
-    //GreekWord antepenult =  proparoxytone.accent(AccentPattern.RECESSIVE)
-    //assert antepenult.toString() == expectedProparoxytone
+    GreekWord antepenult =  proparoxytone.accent(AccentPattern.RECESSIVE)
+    assert antepenult.toString() == expectedProparoxytone
   }
 }
