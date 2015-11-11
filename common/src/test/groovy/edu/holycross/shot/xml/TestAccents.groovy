47fcccdd28bcd4a35acc9b8fd281b68ed5474498
Neel Smith <dnsmith.neel@gmail.com>
2015-11-11 11:52:55 -0500

    Add test file

---
+++ common/src/test/groovy/edu/holycross/shot/xml/TestAccents.groovy
@@ -0,0 +1,15 @@
+package edu.holycross.shot.xml
+
+import org.junit.Test
+import static groovy.test.GroovyAssert.shouldFail
+
+import edu.holycross.shot.orthography.GreekWord
+import edu.holycross.shot.orthography.GreekString
+
+class TestGreekNode {
+
+
+  @Test
+  void testNode() {
+  }
+}
