9991a8367daa79ada92df240edb293704bf8ca00
Neel Smith <dnsmith.neel@gmail.com>
2015-11-26 08:57:38 -0500

    Add templates for 1st declen. nouns

---
+++ morphology/src/test/groovy/edu/holycross/shot/greekmorph/nouns/TestXwra.groovy
@@ -0,0 +1,16 @@
+package edu.holycross.shot.greekmorph
+
+import edu.holycross.shot.orthography.GreekString
+
+import org.junit.Test
+import static groovy.test.GroovyAssert.shouldFail
+
+
+/** Tests demonstrating parsing of nouns from Unicode string.
+*/
+class TestXwra {
+
+   @Test
+   void testDeclension(){}
+
+}
