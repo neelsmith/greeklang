6b8aece04becb3e7b0474714acf13abca57d8a42
Neel Smith <dnsmith.neel@gmail.com>
2015-11-26 04:24:25 -0500

    Test full declension

--- morphology/src/test/groovy/edu/holycross/shot/greekmorph/nouns/TestAnqrwpos.groovy
+++ morphology/src/test/groovy/edu/holycross/shot/greekmorph/nouns/TestAnqrwpos.groovy
@@ -11,28 +11,80 @@ import static groovy.test.GroovyAssert.shouldFail
 class TestAnqrwpos {
 
   // External files used in didactic tests:
-  // FST toolkit's batch parser:
-  String fstinfl = "/usr/bin/fst-infl"
+  //
   // CSV files with URN abbreviations for stems and inflectional rules
   File lexCsvSource = new File("sampledata/urn-registries/datasets.csv")
   File inflCsvSource = new File("src/fst/collectionAbbreviations.csv")
-
+  // A URN manager configured with CITE collection abbreviations
+  // for both inflectional patterns and lexicon of stems:
+  UrnManager umgr = new UrnManager(inflCsvSource)
 
   // Compiled finite state transducer:
   String fstBinary = "build/fst/greek.a"
 
+
+
+  @Test
+  void testDeclension() {
+    // Add lexicon to URN manager:
+    umgr.addCsvFile(lexCsvSource)
+    // And, finally, the parser:
+    MorphologicalParser mp = new MorphologicalParser(fstBinary, umgr)
+    // map keyed by forms to analyze, to a unique GCN of noun form
+    def expectedUnique = [
+    "ἄνθρωπος": [Gender.MASCULINE, GrammaticalCase.NOMINATIVE, GrammaticalNumber.SINGULAR],
+    "ἀνθρώπου": [Gender.MASCULINE, GrammaticalCase.GENITIVE, GrammaticalNumber.SINGULAR],
+    "ἀνθρώπῳ": [Gender.MASCULINE, GrammaticalCase.DATIVE, GrammaticalNumber.SINGULAR],
+    "ἄνθρωπον": [Gender.MASCULINE, GrammaticalCase.ACCUSATIVE, GrammaticalNumber.SINGULAR],
+
+    "ἀνθρώπων": [Gender.MASCULINE, GrammaticalCase.GENITIVE, GrammaticalNumber.PLURAL],
+    "ἀνθρώποις": [Gender.MASCULINE, GrammaticalCase.DATIVE, GrammaticalNumber.PLURAL],
+    "ἀνθρώπους": [Gender.MASCULINE, GrammaticalCase.ACCUSATIVE, GrammaticalNumber.PLURAL],
+
+    "ἄνθρωπε": [Gender.MASCULINE, GrammaticalCase.VOCATIVE, GrammaticalNumber.SINGULAR]
+    ]
+
+    expectedUnique.keySet().each { greek ->
+      def expectedAnswer = expectedUnique[greek]
+      MorphologicalAnalysis morph = mp.parseGreekString(new GreekString(greek,true))
+      assert morph.analyses.size() == 1
+      MorphForm form = morph.analyses[0].getMorphForm()
+      assert form.getAnalyticalType() == AnalyticalType.NOUN
+      CitableId formIdentification = form.getAnalysis()
+      assert formIdentification.getGender() == expectedAnswer[0]
+      assert formIdentification.getCas() == expectedAnswer[1]
+      assert formIdentification.getNum() == expectedAnswer[2]
+    }
+
+    // Check also the ambiguous nom/voc form.
+    def nom_voc = [GrammaticalCase.NOMINATIVE,GrammaticalCase.VOCATIVE ]
+    GreekString ambiguous = new GreekString("ἄνθρωποι",true)
+    MorphologicalAnalysis morph = mp.parseGreekString(ambiguous)
+    assert morph.analyses.size() == 2
+    morph.analyses.each {
+        MorphForm form = it.getMorphForm()
+        assert form.getAnalyticalType() == AnalyticalType.NOUN
+        CitableId formIdentification = form.getAnalysis()
+        // can't know ordering of analyses, but case must be
+        // ONE of these two!
+        assert nom_voc.contains(formIdentification.getCas())
+        assert formIdentification.getGender() == Gender.MASCULINE
+        assert formIdentification.getNum() == GrammaticalNumber.PLURAL
+    }
+  }
+
+
+
   @Test
   void testParserDidactically() {
+    // Add lexicon to URN manager:
+    umgr.addCsvFile(lexCsvSource)
+    // And, finally, the parser:
+    MorphologicalParser mp = new MorphologicalParser(fstBinary, umgr)
+
     String testWord = "ἀνθρώπου"
     GreekString s = new GreekString(testWord, true)
 
-    // A URN manager configured with CITE collection abbreviations
-    // for both inflectional patterns and lexicon of stems:
-    UrnManager umgr = new UrnManager(inflCsvSource)
-    umgr.addCsvFile(lexCsvSource)
-    MorphologicalParser mp = new MorphologicalParser(fstBinary, umgr)
-    mp.debug = 10
-    mp.fstParser.debug = 10
     // Parsing a GreekString gets you 0 or more analyses
     MorphologicalAnalysis morph = mp.parseGreekString(s)
     // although there is only 1 possibility for ἀνθρώπου.
@@ -60,7 +112,6 @@ class TestAnqrwpos {
       String expectedStemExplanation =  "urn:cite:gmorph:coretests.n8909_0"
       assert explanation.stem.toString() == expectedStemExplanation
 
-      System.err.println "antth. stem expl: " + explanation.stem.toString()
       // Inflectional patterns are explained by a URN identifying the
       // the inflectional rule applied to the stem
       String expectedInflectionExplanation = "urn:cite:gmorph:nouninfl.os_ou2"
