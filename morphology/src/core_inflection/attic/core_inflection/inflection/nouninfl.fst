37957760380e5a912cafc78427209910b17696bd
Neel Smith <dnsmith.neel@gmail.com>
2015-12-29 13:33:08 -0500

    Test 2nd decl in Attic ortho

--- morphology/src/core_inflection/attic/core_inflection/inflection/nouninfl.fst
+++ morphology/src/core_inflection/attic/core_inflection/inflection/nouninfl.fst
@@ -13,11 +13,29 @@ $h_hs_ending$ = <h_hs> (<u>nouninfl\.h\_hs1</u>E[<masc><fem>]<nom><sg> |\
 <u>nouninfl\.h\_hs10</u>AI[<masc><fem>]<voc><pl> \
 )
 %
-
-
 $decl1noun_ending$ = $h_hs_ending$
 
 
+
+$os_ou_ending$ = <os_ou> (<u>nouninfl\.os\_ou1</u>O<sh>S[<masc><fem>]<nom><sg> |\
+  <u>nouninfl\.os\_ou14</u>O<sh>N<neut><nom><sg> |\
+  <u>nouninfl\.os\_ou2</u>O<lo>[<masc><fem><neut>]<gen><sg> |\
+  <u>nouninfl\.os\_ou3</u>O<lo>I[<masc><fem><neut>]<dat><sg> |\
+  <u>nouninfl\.os\_ou4</u>O<sh>N[<masc><fem><neut>]<acc><sg> |\
+  <u>nouninfl\.os\_ou5</u>E<sh>[<masc><fem>]<voc><sg> |\
+  <u>nouninfl\.os\_ou6</u>OI[<masc><fem>]<nom><pl> |\
+  <u>nouninfl\.os\_ou15</u>A<neut><nom><pl> |\
+  <u>nouninfl\.os\_ou7</u>O<lo>N[<masc><fem><neut>]<gen><pl> |\
+  <u>nouninfl\.os\_ou8</u>OIS[<masc><fem><neut>]<dat><pl> |\
+  <u>nouninfl\.os\_ou9</u>O<lo>S[<masc><fem>]<acc><pl> |\
+    <u>nouninfl\.os\_ou10</u>A<neut><acc><pl>  |\
+    <u>nouninfl\.os\_ou11</u>O<lo>N[<neut>]<voc><sg> |\
+    <u>nouninfl\.os\_ou12</u>OI[<masc><fem>]<voc><pl> |\
+    <u>nouninfl\.os\_ou13</u>A[<neut>]<voc><pl> \
+)
+
+$decl2noun_ending$ = $os_ou_ending$
+
 % Sum it all up:
-$nouninfll$ = $decl1noun_ending$
+$nouninfll$ = $decl1noun_ending$ | $decl2noun_ending$
 $nouninfll$
---
+++ morphology/src/test/groovy/edu/holycross/shot/greekmorph/nouns/attic/TestDoronAttic.groovy
@@ -0,0 +1,77 @@
+package edu.holycross.shot.greekmorph
+
+import edu.holycross.shot.attic.AtticString
+
+import org.junit.Test
+import static groovy.test.GroovyAssert.shouldFail
+
+
+/** Tests demonstrating parsing of nouns from Unicode string.
+*/
+class TestDoronAttic {
+
+  // External files used in didactic tests:
+  // FST toolkit's batch parser:
+  String fstinfl = "/usr/bin/fst-infl"
+  // CSV files with URN abbreviations for stems and inflectional rules
+  File lexCsvSource = new File("sampledata/urn-registries/datasets.csv")
+  File inflCsvSource = new File("src/fst/collectionAbbreviations.csv")
+  // A URN manager configured with CITE collection abbreviations
+  // for both inflectional patterns and lexicon of stems:
+  UrnManager umgr = new UrnManager(inflCsvSource)
+
+  // Compiled finite state transducer:
+  String atticBinary = "build/attic/greek.a"
+
+  @Test
+  void testDeclension() {
+    // Add lexicon to URN manager:
+    umgr.addCsvFile(lexCsvSource)
+    // And, finally, the parser:
+    AtticGreekParser agp = new AtticGreekParser(atticBinary, umgr)
+    agp.debug = 10
+    agp.fstParser.debug = 10
+
+    // map keyed by forms to analyze, to a unique GCN of noun form
+    def expectedUnique = [
+
+    "DORO": [Gender.NEUTER, GrammaticalCase.GENITIVE, GrammaticalNumber.SINGULAR],
+    "DOROI": [Gender.NEUTER, GrammaticalCase.DATIVE, GrammaticalNumber.SINGULAR],
+
+
+
+    "DOROIS": [Gender.NEUTER, GrammaticalCase.DATIVE, GrammaticalNumber.PLURAL],
+
+    ]
+
+    expectedUnique.keySet().each { greek ->
+      def expectedAnswer = expectedUnique[greek]
+      MorphologicalAnalysis morph = agp.parseAtticString(new AtticString(greek))
+      assert morph.analyses.size() == 1
+      MorphForm form = morph.analyses[0].getMorphForm()
+      assert form.getAnalyticalType() == AnalyticalType.NOUN
+      CitableId formIdentification = form.getAnalysis()
+      assert formIdentification.getGender() == expectedAnswer[0]
+      assert formIdentification.getCas() == expectedAnswer[1]
+      assert formIdentification.getNum() == expectedAnswer[2]
+    }
+
+
+    // Check also the ambiguous nom/voc form.
+    def nom_acc_voc = [GrammaticalCase.NOMINATIVE,GrammaticalCase.ACCUSATIVE,GrammaticalCase.VOCATIVE ]
+    AtticString ambiguous = new AtticString("DORA")
+    MorphologicalAnalysis morph = agp.parseAtticString(ambiguous)
+    assert morph.analyses.size() == 3
+    morph.analyses.each {
+        MorphForm form = it.getMorphForm()
+        assert form.getAnalyticalType() == AnalyticalType.NOUN
+        CitableId formIdentification = form.getAnalysis()
+        // can't know ordering of analyses, but case must be
+        // ONE of these three:
+        assert nom_acc_voc.contains(formIdentification.getCas())
+        assert formIdentification.getGender() == Gender.NEUTER
+        assert formIdentification.getNum() == GrammaticalNumber.PLURAL
+    }
+  }
+
+}
