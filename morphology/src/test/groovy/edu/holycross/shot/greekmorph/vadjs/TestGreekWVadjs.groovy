6159dff184743d8e70b7776fd06c5c42860ced8b
Neel Smith <dnsmith.neel@gmail.com>
2016-03-21 12:10:20 -0400

    Add more test outlines

---
+++ morphology/src/test/groovy/edu/holycross/shot/greekmorph/vadjs/TestGreekWVadjs.groovy
@@ -0,0 +1,26 @@
+package edu.holycross.shot.greekmorph
+
+import edu.holycross.shot.orthography.GreekString
+
+import org.junit.Test
+import static groovy.test.GroovyAssert.shouldFail
+
+
+/** Tests demonstrating parsing of nouns from Unicode string.
+*/
+class TestGreekWPresent {
+  String fstBinary = "build/smyth/greek.a"
+  File urnReg = new File("sampledata/smyth/urnregistry/collectionregistry.csv")
+  UrnManager umgr = new UrnManager(urnReg)
+  // The parser:
+  LiteraryGreekParser mp = new LiteraryGreekParser(fstBinary, umgr)
+
+  @Test
+  void testPresParticiple() {
+    mp.debug = 10
+    mp.fstParser.debug = 10
+
+
+  }
+
+}
