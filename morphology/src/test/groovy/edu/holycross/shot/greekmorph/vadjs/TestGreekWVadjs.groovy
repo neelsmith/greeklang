db84e19bab726afa2e38040a2efa126b396af08e
Neel Smith <dnsmith.neel@gmail.com>
2016-03-22 09:07:55 -0400

    Pass unit tests

--- morphology/src/test/groovy/edu/holycross/shot/greekmorph/vadjs/TestGreekWVadjs.groovy
+++ morphology/src/test/groovy/edu/holycross/shot/greekmorph/vadjs/TestGreekWVadjs.groovy
@@ -8,7 +8,7 @@ import static groovy.test.GroovyAssert.shouldFail
 
 /** Tests demonstrating parsing of nouns from Unicode string.
 */
-class TestGreekWPresent {
+class TestGreekWVadjs {
   String fstBinary = "build/smyth/greek.a"
   File urnReg = new File("sampledata/smyth/urnregistry/collectionregistry.csv")
   UrnManager umgr = new UrnManager(urnReg)
@@ -19,8 +19,18 @@ class TestGreekWPresent {
   void testPresParticiple() {
     mp.debug = 10
     mp.fstParser.debug = 10
+    String greek = "λυτέος"
+    MorphologicalAnalysis morph = mp.parseGreekString(new GreekString(greek,true))
+    // could be middle, could be passive
+    assert morph.analyses.size() == 1
 
 
+    MorphForm form = morph.analyses[0].getMorphForm()
+    assert form.getAnalyticalType() == AnalyticalType.VERBAL_ADJECTIVE
+    CitableId formIdentification = form.getAnalysis()
+    assert formIdentification.getGender() == Gender.MASCULINE
+    assert formIdentification.getCas() == GrammaticalCase.NOMINATIVE
+    assert formIdentification.getNum() == GrammaticalNumber.SINGULAR
   }
 
 }
