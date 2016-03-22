0390b764d11f9af4f04ca6f351596dbf884effac
Neel Smith <dnsmith.neel@gmail.com>
2016-03-22 15:07:08 -0400

    Pass unit tests

--- morphology/src/test/groovy/edu/holycross/shot/greekmorph/fstaps/TestFstAPIrregNoun.groovy
+++ morphology/src/test/groovy/edu/holycross/shot/greekmorph/fstaps/TestFstAPIrregNoun.groovy
@@ -13,20 +13,19 @@ class TestFstAPIrregNoun {
   void testParser() {
     File urnReg = new File("sampledata/smyth/urnregistry/collectionregistry.csv")
     umgr.addCsvFile(urnReg)
-    String noun = "<u>smyth.n29828_0</u><u>lexent.n29828</u>dwr<noun><neut><os_ou><stempenacc>::<os_ou><noun>ou<neut><gen><sg><u>nouninfl.os_ou2n</u>"
-
-    FstAnalysisParser fap = new FstAnalysisParser(noun, umgr)
+    String irreg = "<u>smyth.n23069_0</u><u>lexent.n23069</u>gunaiko/s<fem><gen><sg><irregnoun>::<irregnoun><noun><u>irreginfl.1</u>"
+    FstAnalysisParser fap = new FstAnalysisParser(irreg, umgr)
 
     assert fap.analysisPattern == AnalyticalType.NOUN
-    assert fap.lexicalEntity.toString() == "urn:cite:shot:lexent.n29828"
-    assert fap.explanation.stem.toString() == "urn:cite:gmorph:smyth.n29828_0"
-    assert fap.explanation.inflection.toString() == "urn:cite:gmorph:nouninfl.os_ou2n"
-    assert fap.getSurfaceStem() == "dwr"
-    assert fap.getSurfaceInflection() == "ou"
-    assert fap.getSurface() == "dwr-ou"
+    assert fap.lexicalEntity.toString() == "urn:cite:shot:lexent.n23069"
+    assert fap.explanation.stem.toString() == "urn:cite:gmorph:smyth.n23069_0"
+    assert fap.explanation.inflection.toString() == "urn:cite:gmorph:irreginfl.1"
+    System.err.println "Surface stem: " + fap.getSurfaceStem()
+    System.err.println  "inflection: " +  fap.getSurfaceInflection()
+    System.err.println "Surface: " +  fap.getSurface()
 
     MorphForm mf = fap.getMorphForm()
-    assert mf.toString() == "noun: neuter genitive singular"
+    assert mf.toString() == "noun: feminine genitive singular"
   }
 
 }
