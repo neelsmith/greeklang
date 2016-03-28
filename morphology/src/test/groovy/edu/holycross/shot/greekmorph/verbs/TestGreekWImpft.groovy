8d35059da638685d09aadd6d712ba1a7ce5a52a6
Neel Smith <dnsmith.neel@gmail.com>
2016-03-27 20:45:35 -0400

    Pass all forms in sing and pl

--- morphology/src/test/groovy/edu/holycross/shot/greekmorph/verbs/TestGreekWImpft.groovy
+++ morphology/src/test/groovy/edu/holycross/shot/greekmorph/verbs/TestGreekWImpft.groovy
@@ -38,17 +38,20 @@ class TestGreekWImpft {
 
   }
 
-/*
   @Test
-  void testActive() {
-    //
+  void testUnique () {
+    mp.debug = 10
+    mp.fstParser.debug = 10
     def expectedUnique = [
-    "λύομεν": [Person.FIRST, GrammaticalNumber.PLURAL, Tense.PRESENT, Mood.INDICATIVE, Voice.ACTIVE]
+    "ἔλυες": [Person.SECOND, GrammaticalNumber.SINGULAR, Tense.IMPERFECT, Mood.INDICATIVE, Voice.ACTIVE],
+    "ἔλυε": [Person.THIRD, GrammaticalNumber.SINGULAR, Tense.IMPERFECT, Mood.INDICATIVE, Voice.ACTIVE],
+
+    "ἐλύομεν": [Person.FIRST, GrammaticalNumber.PLURAL, Tense.IMPERFECT, Mood.INDICATIVE, Voice.ACTIVE],
+    "ἐλύετε": [Person.SECOND, GrammaticalNumber.PLURAL, Tense.IMPERFECT, Mood.INDICATIVE, Voice.ACTIVE]
     ]
     expectedUnique.keySet().each { greek ->
       def expectedAnswer = expectedUnique[greek]
       MorphologicalAnalysis morph = mp.parseGreekString(new GreekString(greek,true))
-
       assert morph.analyses.size() == 1
       MorphForm form = morph.analyses[0].getMorphForm()
       assert form.getAnalyticalType() == AnalyticalType.CVERB
@@ -58,8 +61,63 @@ class TestGreekWImpft {
       assert formIdentification.getTense() == expectedAnswer[2]
       assert formIdentification.getMood() == expectedAnswer[3]
       assert formIdentification.getVoice() == expectedAnswer[4]
+
+    }
+  }
+
+
+
+  @Test
+  void testFirstThird() {
+    String greek = "ἔλυον"
+    MorphologicalAnalysis morph = mp.parseGreekString(new GreekString(greek,true))
+    assert morph.analyses.size() == 2
+    morph.analyses.each { ma ->
+      MorphForm form = ma.getMorphForm()
+      assert form.getAnalyticalType() == AnalyticalType.CVERB
+
+      CitableId formIdentification = form.getAnalysis()
+      assert formIdentification.getTense() == Tense.IMPERFECT
+      assert formIdentification.getMood() == Mood.INDICATIVE
+      assert formIdentification.getVoice() ==  Voice.ACTIVE
+
+      if (formIdentification.getPerson() == Person.FIRST) {
+	assert formIdentification.getNum() == GrammaticalNumber.SINGULAR
+      } else {
+	assert formIdentification.getNum() == GrammaticalNumber.PLURAL
+	assert formIdentification.getPerson() == Person.THIRD
+      }
+    }
+  }
+
+  // add test on duals
+  
+  @Test
+  void testMP() {
+    def expectedMP = [
+      "ἐλυόμην": [Person.FIRST, GrammaticalNumber.SINGULAR, Tense.IMPERFECT, Mood.INDICATIVE],
+      "ἐλύου": [Person.SECOND, GrammaticalNumber.SINGULAR, Tense.IMPERFECT, Mood.INDICATIVE],
+      "ἐλύετο": [Person.THIRD, GrammaticalNumber.SINGULAR, Tense.IMPERFECT, Mood.INDICATIVE],
+      "ἐλυόμεθα": [Person.FIRST, GrammaticalNumber.PLURAL, Tense.IMPERFECT, Mood.INDICATIVE],
+      "ἐλύεσθε": [Person.SECOND, GrammaticalNumber.PLURAL, Tense.IMPERFECT, Mood.INDICATIVE],
+      "ἐλύοντο" : [Person.THIRD, GrammaticalNumber.PLURAL, Tense.IMPERFECT, Mood.INDICATIVE],
+    ]
+
+    expectedMP.keySet().each { greek ->
+      def expectedAnswer = expectedMP[greek]
+      MorphologicalAnalysis morph = mp.parseGreekString(new GreekString(greek,true))
+      assert morph.analyses.size() == 2
+      MorphForm form = morph.analyses[0].getMorphForm()
+      assert form.getAnalyticalType() == AnalyticalType.CVERB
+      CitableId formIdentification = form.getAnalysis()
+      assert formIdentification.getPerson() == expectedAnswer[0]
+      assert formIdentification.getNum() == expectedAnswer[1]
+      assert formIdentification.getTense() == expectedAnswer[2]
+      assert formIdentification.getMood() == expectedAnswer[3]
+      assert ((formIdentification.getVoice() == Voice.MIDDLE) || (formIdentification.getVoice() == Voice.PASSIVE))
+
     }
 
-
-  } */
+    
+  }
 }
