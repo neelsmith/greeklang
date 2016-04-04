32cab0b128e6fa9a2040ffc6cffa92b7400de11d
Neel Smith <dnsmith.neel@gmail.com>
2016-04-04 15:42:16 -0400

    Begin testing infinitives

--- morphology/src/test/groovy/edu/holycross/shot/greekmorph/infinitives/TestGreekWInfinitives.groovy
+++ morphology/src/test/groovy/edu/holycross/shot/greekmorph/infinitives/TestGreekWInfinitives.groovy
@@ -8,40 +8,35 @@ import static groovy.test.GroovyAssert.shouldFail
 
 /** Tests demonstrating parsing of nouns from Unicode string.
 */
-class TestGreekWInfinitives {
+class TestGreekWInfins {
   String fstBinary = "build/smyth/greek.a"
   File urnReg = new File("sampledata/smyth/urnregistry/collectionregistry.csv")
   UrnManager umgr = new UrnManager(urnReg)
   // The parser:
   LiteraryGreekParser mp = new LiteraryGreekParser(fstBinary, umgr)
 
-  @Test
-  void testSinglePrelim() {
-    mp.debug = 10
-    mp.fstParser.debug = 10
-
-
-    String greek = "λύειν"
-    MorphologicalAnalysis morph = mp.parseGreekString(new GreekString(greek,true))
-    assert morph.analyses.size() == 1
-
-    MorphForm form = morph.analyses[0].getMorphForm()
-    assert form.getAnalyticalType() == AnalyticalType.INFINITIVE
-    CitableId formIdentification = form.getAnalysis()
-    assert formIdentification.getTense() == Tense.PRESENT
-    assert formIdentification.getVoice() == Voice.ACTIVE
-  }
 
   @Test
-  void testActive() {
-    mp.debug = 10
-    mp.fstParser.debug = 10
-    def expectedTense = [
-    "λύειν": [Tense.PRESENT],
-    "λύσειν": [Tense.FUTURE],
-    "λῦσαι": [Tense.AORIST],
-    "λελυκέναι": [Tense.PERFECT]
+  void testUnique() {
+    def expectedUnique = [
+    "λύειν": [Tense.PRESENT, Voice.ACTIVE],
+    "λύσειν": [Tense.FUTURE, Voice.ACTIVE],
+    "λύσεσθαι": [Tense.FUTURE, Voice.MIDDLE],
+    "λύσασθαι": [Tense.AORIST, Voice.MIDDLE],
+    "λυθήσεσθαι": [Tense.FUTURE, Voice.PASSIVE],
+    "λυθῆναι": [Tense.AORIST, Voice.PASSIVE],
     ]
+    expectedUnique.keySet().each { greek ->
+      def expectedAnswer = expectedUnique[greek]
+      MorphologicalAnalysis morph = mp.parseGreekString(new GreekString(greek,true))
+
+      assert morph.analyses.size() == 1
+      MorphForm form = morph.analyses[0].getMorphForm()
+      assert form.getAnalyticalType() == AnalyticalType.CVERB
+      CitableId formIdentification = form.getAnalysis()
+      assert formIdentification.getTense() == expectedAnswer[0]
+      assert formIdentification.getVoicew() == expectedAnswer[1]
+    }
   }
 
 }
