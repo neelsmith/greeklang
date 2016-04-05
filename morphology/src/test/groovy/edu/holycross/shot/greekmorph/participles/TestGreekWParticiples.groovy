8a41abfe933eb947e34175b95e8b75501e09632f
Neel Smith <dnsmith.neel@gmail.com>
2016-04-05 17:20:33 -0400

    Set up testing on participles

--- morphology/src/test/groovy/edu/holycross/shot/greekmorph/participles/TestGreekWParticiples.groovy
+++ morphology/src/test/groovy/edu/holycross/shot/greekmorph/participles/TestGreekWParticiples.groovy
@@ -15,28 +15,32 @@ class TestGreekWParticiples {
   // The parser:
   LiteraryGreekParser mp = new LiteraryGreekParser(fstBinary, umgr)
 
+
   @Test
-  void testPresParticiple() {
-    mp.debug = 10
-    mp.fstParser.debug = 10
+  void testUnique() {
+    def expectedUnique = [
+    "λύων": [Gender.MASCULINE, GrammaticalCase.NOMINATIVE, GrammaticalNumber.SINGULAR, Tense.PRESENT, Voice.ACTIVE],
+    "λύσων": [Gender.MASCULINE, GrammaticalCase.NOMINATIVE, GrammaticalNumber.SINGULAR, Tense.FUTURE, Voice.ACTIVE],
+    "λύσας": [Gender.MASCULINE, GrammaticalCase.NOMINATIVE, GrammaticalNumber.SINGULAR, Tense.AORIST, Voice.ACTIVE],
+    "λελυκώς": [Gender.MASCULINE, GrammaticalCase.NOMINATIVE, GrammaticalNumber.SINGULAR, Tense.PERFECT, Voice.ACTIVE],
 
+    "λυσόμενος": [Gender.MASCULINE, GrammaticalCase.NOMINATIVE, GrammaticalNumber.SINGULAR, Tense.FUTURE, Voice.MIDDLE],
+    "λυσάμενος": [Gender.MASCULINE, GrammaticalCase.NOMINATIVE, GrammaticalNumber.SINGULAR, Tense.AORIST, Voice.MIDDLE],
 
+    "λυθησόμενος": [Gender.MASCULINE, GrammaticalCase.NOMINATIVE, GrammaticalNumber.SINGULAR, Tense.FUTURE, Voice.PASSIVE],
+    "λυθείς": [Gender.MASCULINE, GrammaticalCase.NOMINATIVE, GrammaticalNumber.SINGULAR, Tense.AORIST, Voice.PASSIVE],
+    ]
+    expectedUnique.keySet().each { greek ->
+      System.err.println "GREEK " + greek
+      def expectedAnswer = expectedUnique[greek]
+      MorphologicalAnalysis morph = mp.parseGreekString(new GreekString(greek,true))
 
-    String greek = "λύων"
-    MorphologicalAnalysis morph = mp.parseGreekString(new GreekString(greek,true))
-    assert morph.analyses.size() == 1
-
-
-
-
-    MorphForm form = morph.analyses[0].getMorphForm()
-    assert form.getAnalyticalType() == AnalyticalType.PARTICIPLE
-    CitableId formIdentification = form.getAnalysis()
-    assert formIdentification.getGender() == Gender.MASCULINE
-    assert formIdentification.getCas() == GrammaticalCase.NOMINATIVE
-    assert formIdentification.getNum() == GrammaticalNumber.SINGULAR
-    assert formIdentification.getTense() == Tense.PRESENT
-    assert formIdentification.getVoice() == Voice.ACTIVE
+      assert morph.analyses.size() == 1
+      MorphForm form = morph.analyses[0].getMorphForm()
+    }
   }
+
+// MP forms:
+// luomenos, lelumenos
 
 }
