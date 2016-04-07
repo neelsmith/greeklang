f8126494795b90927aca134c818b8504e7b94944
Neel Smith <dnsmith.neel@gmail.com>
2016-04-07 07:17:43 -0400

    Reorganize tests for Smyth sections by Smyth section

---
+++ morphology/src/test/groovy/edu/holycross/shot/greekmorph/smyth/verbs/smyth406/TestGreekMPP5Plupft.groovy
@@ -0,0 +1,57 @@
+package edu.holycross.shot.greekmorph
+
+import edu.holycross.shot.orthography.GreekString
+
+import org.junit.Test
+import static groovy.test.GroovyAssert.shouldFail
+
+
+/** Smyth 406.
+*/
+class TestGreekMPP5Plupft {
+  String fstBinary = "build/smyth/greek.a"
+  File urnReg = new File("sampledata/smyth/urnregistry/collectionregistry.csv")
+  UrnManager umgr = new UrnManager(urnReg)
+  // The parser:
+  LiteraryGreekParser mp = new LiteraryGreekParser(fstBinary, umgr)
+
+
+
+  @Test
+  void testUnique() {
+    //
+    def expectedUnique = [
+    "ἐγεγράμμην": [Person.FIRST, GrammaticalNumber.SINGULAR, Tense.PLUPERFECT, Mood.INDICATIVE],
+    "ἐγέγραψο": [Person.SECOND, GrammaticalNumber.SINGULAR, Tense.PLUPERFECT, Mood.INDICATIVE],
+    //"ἐγέγραπτο": [Person.THIRD, GrammaticalNumber.SINGULAR, Tense.PLUPERFECT, Mood.INDICATIVE],
+
+
+    "ἐγέγραφθον": [Person.SECOND, GrammaticalNumber.DUAL, Tense.PLUPERFECT, Mood.INDICATIVE],
+    "ἐγεγράφθην": [Person.THIRD, GrammaticalNumber.DUAL, Tense.PLUPERFECT, Mood.INDICATIVE],
+
+    "ἐγεγράμμεθα": [Person.FIRST, GrammaticalNumber.PLURAL, Tense.PLUPERFECT, Mood.INDICATIVE],
+    "ἐγέγραφθε": [Person.SECOND, GrammaticalNumber.PLURAL, Tense.PLUPERFECT, Mood.INDICATIVE]
+
+    ]
+    expectedUnique.keySet().each { greek ->
+      def expectedAnswer = expectedUnique[greek]
+      MorphologicalAnalysis morph = mp.parseGreekString(new GreekString(greek,true))
+
+      assert morph.analyses.size() == 2
+      morph.analyses.each { ma ->
+
+        MorphForm form = ma.getMorphForm()
+        assert form.getAnalyticalType() == AnalyticalType.CVERB
+        CitableId formIdentification = form.getAnalysis()
+        assert formIdentification.getPerson() == expectedAnswer[0]
+        assert formIdentification.getNum() == expectedAnswer[1]
+        assert formIdentification.getTense() == expectedAnswer[2]
+        assert formIdentification.getMood() == expectedAnswer[3]
+        assert [Voice.MIDDLE, Voice.PASSIVE].contains(formIdentification.getVoice())
+      }
+    }
+  }
+
+
+
+}
