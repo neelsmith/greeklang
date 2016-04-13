009fe4e00b4016f19b9b837ed0a86b7996ecfc73
Neel Smith <dnsmith.neel@gmail.com>
2016-04-13 09:13:29 -0400

    Add tests for pluperfect

--- morphology/src/test/groovy/edu/holycross/shot/greekmorph/corpus/smyth/verbs/smyth406/TestSmyth406.groovy
+++ morphology/src/test/groovy/edu/holycross/shot/greekmorph/corpus/smyth/verbs/smyth406/TestSmyth406.groovy
@@ -14,12 +14,18 @@ class TestSmyth406 {
       File urnReg = new File("sampledata/smyth/urnregistry/collectionregistry.csv")
       UrnManager umgr = new UrnManager(urnReg)
       // The parser:
-      LiteraryGreekParser mp = new LiteraryGreekParser(fstBinary, umgr, 9)
+      LiteraryGreekParser mp = new LiteraryGreekParser(fstBinary, umgr, 8)
       TableTester tester = new TableTester(mp)
 
       @Test
-      void testS382() {
-        File dataFile =  new File("unit_tests_data/smyth/smyth406/smyth406.csv")
+      void testPerfect() {
+        File dataFile =  new File("unit_tests_data/smyth/smyth406/smyth406pft.csv")
+        assert tester.testFile(dataFile)
+      }
+
+      @Test
+      void testPlPerfect() {
+        File dataFile =  new File("unit_tests_data/smyth/smyth406/smyth406plupft.csv")
         assert tester.testFile(dataFile)
       }
 
