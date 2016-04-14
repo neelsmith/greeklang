4f6dbaaa8c1858b790168b11faab967b2b55e7ec
Neel Smith <dnsmith.neel@gmail.com>
2016-04-14 09:34:04 -0400

    Begin testing Smyth 383 present m-p

---
+++ morphology/src/test/groovy/edu/holycross/shot/greekmorph/corpus/smyth/verbs/smyth383/TestSmythW383fut.groovy
@@ -0,0 +1,25 @@
+package edu.holycross.shot.greekmorph
+
+import edu.holycross.shot.orthography.GreekString
+
+import org.junit.Test
+import static groovy.test.GroovyAssert.shouldFail
+
+
+/** Tests demonstrating parsing of nouns from Unicode string.
+*/
+class TestSmythW383fut {
+  String fstBinary = "build/smyth/greek.a"
+  File urnReg = new File("sampledata/smyth/urnregistry/collectionregistry.csv")
+  UrnManager umgr = new UrnManager(urnReg)
+  // The parser:
+  LiteraryGreekParser mp = new LiteraryGreekParser(fstBinary, umgr, 8)
+  TableTester tester = new TableTester(mp)
+
+  @Test
+  void testMP() {
+    File dataFile =  new File("unit_tests_data/smyth/smyth383/smyth383fut.csv")
+    assert tester.testFile(dataFile)
+  }
+
+}
