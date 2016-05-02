6a45c28cd3f8db46f395a953b99cfb48c05d2b33
Neel Smith <dnsmith.neel@gmail.com>
2016-05-02 12:31:28 -0400

    Start to get ptcpls systematically tested

---
+++ morphology/src/test/groovy/edu/holycross/shot/greekmorph/corpus/smyth/participles/smyth305/TestSmythPtcpl305.groovy
@@ -0,0 +1,30 @@
+package edu.holycross.shot.greekmorph
+
+import org.junit.Test
+
+/** .
+*/
+class TestSmythPtcpl305 {
+  String fstBinary = "build/smyth/greek.a"
+  File urnReg = new File("sampledata/smyth/urnregistry/collectionregistry.csv")
+  UrnManager umgr = new UrnManager(urnReg)
+  // The parser:
+  LiteraryGreekParser mp = new LiteraryGreekParser(fstBinary, umgr, 9)
+  // The testing utility:
+  TableTester tester = new TableTester(mp)
+
+  @Test
+  void testLuo() {
+    File dataFile =  new File("unit_tests_data/smyth/smyth305/smyth305-luo.csv")
+    assert tester.testFile(dataFile)
+  }
+/*
+  @Test
+  void testEimi() {
+    File dataFile =  new File("unit_tests_data/smyth/smyth305/psmyth305-eimi.csv")
+    assert tester.testFile(dataFile)
+  }
+  */
+
+
+}
