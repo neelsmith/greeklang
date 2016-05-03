37157548bc67c37e52e6091b76a3d5b2819a66fd
Neel Smith <dnsmith.neel@gmail.com>
2016-05-03 11:52:53 -0400

    Test Smyth 07

---
+++ morphology/src/test/groovy/edu/holycross/shot/greekmorph/corpus/smyth/participles/smyth307/TestSmythPtcpl307.groovy
@@ -0,0 +1,33 @@
+package edu.holycross.shot.greekmorph
+
+import org.junit.Test
+
+/** .
+*/
+class TestSmythPtcpl307 {
+  String fstBinary = "build/smyth/greek.a"
+  File urnReg = new File("sampledata/smyth/urnregistry/collectionregistry.csv")
+  UrnManager umgr = new UrnManager(urnReg)
+  // The parser:
+  LiteraryGreekParser mp = new LiteraryGreekParser(fstBinary, umgr, 9)
+  // The testing utility:
+  TableTester tester = new TableTester(mp)
+/*
+
+  @Test
+  void testLuo() {
+    File dataFile =  new File("unit_tests_data/smyth/smyth307/smyth307-luo.csv")
+    assert tester.testFile(dataFile)
+  }
+*/
+
+  @Test
+  void testHistas() {
+    File dataFile =  new File("unit_tests_data/smyth/smyth307/smyth307-eidws.csv")
+    assert tester.testFile(dataFile)
+  }
+
+
+
+
+}
