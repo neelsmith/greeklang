242e5d87a1ef343a7aebb0aefc95925c1fc6f5f1
Neel Smith <dnsmith.neel@gmail.com>
2016-05-03 14:46:13 -0400

    Test aor. pass.

--- morphology/src/test/groovy/edu/holycross/shot/greekmorph/corpus/smyth/verbs/smyth383/TestSmythW383aor.groovy
+++ morphology/src/test/groovy/edu/holycross/shot/greekmorph/corpus/smyth/verbs/smyth383/TestSmythW383aor.groovy
@@ -24,10 +24,16 @@ class TestSmythW383aor{
   }
 
 
+  @Test
+  void testAorMid() {
+    File dataFile =  new File("unit_tests_data/smyth/smyth383/smyth383aor-mid.csv")
+    assert tester.testFile(dataFile)
+  }
 
-    @Test
-    void testAorMid() {
-      File dataFile =  new File("unit_tests_data/smyth/smyth383/smyth383aor-mid.csv")
-      assert tester.testFile(dataFile)
-    }
+
+  @Test
+  void testAorPass() {
+    File dataFile =  new File("unit_tests_data/smyth/smyth383/smyth383aor-pass.csv")
+    assert tester.testFile(dataFile)
+  }
 }
